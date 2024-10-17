6c1f0bdc4e753c032b9248c141875b3e8808a74b
unknown <jastinafa2016@gmail.com>
2024-10-17 19:04:46 +0330

    add partner for price

---
+++ partner/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from catalouge.models import Product
+
+
+class Partner(models.Model):
+    name = models.CharField(max_length=48)
+    
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+    
+class PartnerStock(models.Model):
+    partner = models.ForeignKey(Partner, related_name="partner", on_delete=models.Cascade)
+    product = models.ForeignKey(Product, related_name="partner_product", on_delete=models.CASCADE)
+    price = models.PositiveBigIntegerField(default=0)
+    
+    
+    def __str__(self):
+        return f"{self.partner} - {self.product} - {self.price}"
+    
+    
