a0df1343384f78ade548df3b22143a6e504b3c42
yas3in <jastinafa2016@gmail.com>
2025-06-11 00:49:41 +0330

    work on customuser model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -8,34 +8,28 @@ from django.utils.translation import gettext_lazy as _
 class MyManager(BaseUserManager):
 
 
-    def create_user(self, phone_number, password=None):
+    def create_user(self, phone_number, password=None, **extra_fields):
         """
         Creates and saves a User with the given email, date of
         birth and password.
         """
         if not phone_number:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(
-            phone_number=phone_number,
-        )
-
+            raise ValueError('The Phone number must be set')  # چون phone_number هست، می‌تونی این خط رو حذف یا تغییر بدی
+        user = self.model(phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, phone_number, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            phone_number=phone_number,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone_number, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser):
@@ -43,7 +37,7 @@ class CustomUser(AbstractBaseUser):
     first_name = models.CharField(_("first name"), max_length=48, blank=True)
     last_name = models.CharField(_("last name"), max_length=48, blank=True)
     email = models.EmailField(_("email"), blank=True)
-    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
@@ -62,7 +56,7 @@ class CustomUser(AbstractBaseUser):
     objects = MyManager()
 
     def __str__(self):
-        return self.email
+        return self.phone_number
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
