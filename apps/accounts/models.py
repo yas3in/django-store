4d82e61b7124804c41996edbffa60d7e154c50bd
yas3in <jastinafa2016@gmail.com>
2025-06-11 12:16:20 +0330

    create custom backend

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -68,11 +68,6 @@ class CustomUser(AbstractBaseUser):
         # Simplest possible answer: Yes, always
         return True
 
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
 
 
 class Sms(models.Model):
--- apps/lib/backend.py
+++ apps/lib/backend.py
@@ -1,10 +1,23 @@
 from django.contrib.auth.backends import BaseBackend
 from random import randint
 
+from apps.accounts.models import CustomUser
+
 
 class SmsBackend(BaseBackend):
-    def send_sms(self):
-        pass
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = CustomUser.objects.get(phone_number=username)
+        except CustomUser.DoesNotExist:
+            return None
+        
+        if user.check_password(password) and user.is_active:
+            return user
+        return None
 
-    def authenticate(self, request, phone, password):
-        pass
+
+    def get_user(self, user_id):
+        try:
+            return CustomUser.objects.get(pk=user_id)
+        except CustomUser.DoesNotExist:
+            return None
